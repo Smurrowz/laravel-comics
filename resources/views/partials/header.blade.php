00448f75d46b7fc4afcbe0de5f1e7fc83d957eea
Smurrowz <nigeldn1995@gmail.com>
2022-10-07 16:38:15 +0200

    fixed

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,54 @@
+<?php 
+$navLinksArray = [
+          [
+            'name' => 'CHARACTERS'
+          ],
+          [
+            'name' => 'COMICS'
+          ],
+          [
+            'name' => 'MOVIES'
+          ],
+          [
+            'name' => 'TV'
+          ],
+          [
+            'name' => 'GAMES'
+          ],
+          [
+            'name' => 'COLLECTIBLES'
+          ],
+          [
+            'name' => 'VIDEOS'
+          ],
+          [
+            'name' => 'FANS'
+          ],
+          [
+            'name' => 'NEWS'
+          ],
+          [
+            'name' => 'SHOP'
+          ],
+        ]
+
+
+
+?>
+
+<header class="container">
+  <div class="logo">
+    <img :src="{{ asset('img/dc-logo.png') }}" alt=" logo dc">
+  </div>
+  <nav class="main-nav">
+    <ul>
+      @foreach ($navLinksArray as $link)
+          <li class="nav-link">
+            <a href="#">{{$link['name']}}</a>
+          </li>
+      @endforeach
+    </ul>
+  </nav>  
+
+ 
+</header>
